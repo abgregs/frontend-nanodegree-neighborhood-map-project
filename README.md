98ea637208433c45105bb5dadb7cb844e7680174
Austin Gregersen <Austin Gregersen>
2017-05-12 13:11:20 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+Front-End Web Development Nano-Degree - Neighborhood Map Project
+================================================================
+
+What is it?
+-----------
+This project uses the Google Maps JavaScript API to create a map of my neighborhood. A list of places will be marked on the map. In this case, coffee shops and other places for food or drinks will be marked. The categories for these places will be included in the app, and when clicked, will show or hide the list of places for that category. It will also include a text input field that filters the map markers and list items to locations matching the text input or selection. It will include a list view of the places on the map, which displays all locations by default, and displays the filtered subset of locations when a search filter is applied. Clicking a place on the list displays unique information about the location, and animates its associated map marker. Clicking a marker displays an info window with unique information about the location.
+
+Getting Started
+---------------
+
+To get started, download the project here: https://github.com/abgregs/frontend-nanodegree-neighborhood-map-project
+
+Load index.html in your browser to view the app (not working yet).
+
+I'm currently just getting started on the project and troubleshooting an issue with getting my places lists organized for each category and for the marker icon to display on the map according to the category. This is a work in progress and will be updated as it moves closer to the final product.
+
+The project currently contains an `index.html` and folders containing `styles.css` and `app.js` respectfully. Within the js folder, the Knockout.js library is also included. This will eventually be used for binding data and refreshing our UI for the search filter and list view features.
---
+++ js/app.js
@@ -0,0 +1,144 @@
+var map;
+
+// Starting coordinates to center on Cap Hill neighborhood.
+var startingLat = 39.7341143;
+var startingLng = -104.9797753;
+
+// Categories for our places.
+var categories = [
+  {name: 'Coffee', iconURL: './img/15.svg', query: 'coffee shops in Capitol Hill, Denver, CO'},
+  {name: 'Pizza', iconURL: './img/23.svg', query: 'pizza in Capitol Hill, Denver, CO'},
+  {name: 'Ice Cream', iconURL: './img/20.svg', query: 'ice cream shops in Capitol Hill, Denver, CO'},
+  {name: 'Breweries', iconURL: './img/14.svg', query: 'breweries in Capitol Hill, Denver, CO'}
+];
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: startingLat, lng: startingLng},
+    zoom: 13,
+    mapTypeControl: false
+  });
+
+  // latLngBounds around Cap Hill neighborhood to bias text search
+  var neBound = new google.maps.LatLng(39.7399521, -104.9730662);
+  var swBound = new google.maps.LatLng(39.7224799, -104.9883449);
+  var bounds = new google.maps.LatLngBounds(neBound, swBound);
+
+  function textSearchPlaces() {
+    var placesService = new google.maps.places.PlacesService(map);
+    for (i=0; i < categories.length; i++) {
+      var query = categories[i].query;
+      placesService.textSearch({
+        query: query,
+        bounds: bounds
+      }, function(results, status) {
+       if (status === google.maps.places.PlacesServiceStatus.OK) {
+         (function(iCopy) {
+            return function () {
+              var name = categories[iCopy].name;
+              var icon = categories[iCopy].iconURL;
+
+             createMarkersForPlaces(results, icon);
+             // Create a place object for each category that includes the text search results.
+
+             var place = new Place(results, name, copy);
+             placeLists.push(place);
+           };
+         }(i));
+       };
+     });
+    }
+ }
+
+  function createMarkersForPlaces(places, categoryIcon) {
+    var bounds = new google.maps.LatLngBounds();
+    for (var i = 0; i < places.length; i++) {
+      var place = places[i];
+      var icon = {
+
+        // Replace icon's url with the iconURL we passed in from our categories array.
+        url: categoryIcon,
+
+        size: new google.maps.Size(40, 40),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(15, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
+      // Create a marker for each place.
+      var marker = new google.maps.Marker({
+        map: map,
+        icon: icon,
+        title: place.name,
+        position: place.geometry.location,
+        id: place.place_id
+      });
+
+      // Create a single infowindow to be used with the place details information
+      // so that only one is open at once.
+      var placeInfoWindow = new google.maps.InfoWindow();
+      // If a marker is clicked, do a place details search on it in the next function.
+      marker.addListener('click', function() {
+        if (placeInfoWindow.marker == this) {
+          console.log("This infowindow already is on this marker!");
+        } else {
+          getPlacesDetails(this, placeInfoWindow);
+        }
+      });
+      // placeMarkers.push(marker);
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    }
+  map.fitBounds(bounds);
+}
+
+// Places details search, executed when a marker is selected
+function getPlacesDetails(marker, infowindow) {
+  var service = new google.maps.places.PlacesService(map);
+  service.getDetails({
+    placeId: marker.id
+  }, function(place, status) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+      // Set the marker property on this infowindow so it isn't created again.
+      infowindow.marker = marker;
+      var innerHTML = '<div>';
+      if (place.name) {
+        innerHTML += '<strong>' + place.name + '</strong>';
+      }
+      if (place.formatted_address) {
+        innerHTML += '<br>' + place.formatted_address;
+      }
+      if (place.photos) {
+        innerHTML += '<br><br><img src="' + place.photos[0].getUrl(
+            {maxHeight: 100, maxWidth: 200}) + '">';
+      }
+      innerHTML += '</div>';
+      infowindow.setContent(innerHTML);
+      infowindow.open(map, marker);
+      // Make sure the marker property is cleared if the infowindow is closed.
+      infowindow.addListener('closeclick', function() {
+        infowindow.marker = null;
+      });
+    }
+  });
+}
+
+textSearchPlaces();
+}
+
+// Place constructor
+function Place (results, name, icon) {
+  this.results = results;
+  this.name = name;
+  this.icon = icon;
+};
+
+// Data model storing our lists of places and categories.
+
+var placeLists = [];
+
+
+setTimeout(function(){ console.log(placeLists); }, 2000);
