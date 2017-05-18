c031f3a19c7b55c19406ca02e007201083250cb5
Austin Gregersen <Austin Gregersen>
2017-05-18 13:43:40 -0600

    create list view and live search filter, add show/hide feature when clicking categories

--- README.md
+++ README.md
@@ -3,15 +3,15 @@ Front-End Web Development Nano-Degree - Neighborhood Map Project
 
 What is it?
 -----------
-This project uses the Google Maps JavaScript API to create a map of my neighborhood. A list of places will be marked on the map. In this case, coffee shops and other places for food or drinks will be marked. The categories for these places will be included in the app, and when clicked, will show or hide the list of places for that category. It will also include a text input field that filters the map markers and list items to locations matching the text input or selection. It will include a list view of the places on the map, which displays all locations by default, and displays the filtered subset of locations when a search filter is applied. Clicking a place on the list displays unique information about the location, and animates its associated map marker. Clicking a marker displays an info window with unique information about the location.
+In this project I used the Google Maps JavaScript API to create a map of my neighborhood. I triggered search requests to generate areas of interest and then mark them on the map. The theme for the areas of interest is food and drink. The categories for the places are included in the app, and when clicked, will show or hide the list of all places belonging to that category. The map also includes a live search feature that filters a list view of the places to display only those places matching the text input. This list view will update with each key stroke of the text entered in the search input. All locations will be displayed on the map and the names listed in the list view by default. Clicking a map marker or the place name in the list view displays unique information about the location in an info window and also animates the selected marker.
 
 Getting Started
 ---------------
 
 To get started, download the project here: https://github.com/abgregs/frontend-nanodegree-neighborhood-map-project
 
-Load index.html in your browser to view the app (not working yet).
+Load index.html in your browser to view the app. Some features may not be created and styles not applied yet as this is still in development.
 
-I'm currently just getting started on the project and troubleshooting an issue with getting my places lists organized for each category and for the marker icon to display on the map according to the category. This is a work in progress and will be updated as it moves closer to the final product.
+This README is a work in progress and will be updated as it moves closer to the final product.
 
-The project currently contains an `index.html` and folders containing `styles.css` and `app.js` respectfully. Within the js folder, the Knockout.js library is also included. This will eventually be used for binding data and refreshing our UI for the search filter and list view features.
+The project currently contains an `index.html` and folders containing `styles.css` and `app.js` respectfully. Within the js folder, the Knockout.js library is also included. This is used for binding data and refreshing our UI based on interactions from the user.
--- js/app.js
+++ js/app.js
@@ -12,33 +12,109 @@ var categories = [
   {name: 'Breweries', iconURL: './img/Brewery.svg', query: 'breweries in Capitol Hill, Denver, CO'}
 ];
 
-// Data model storing our categories and lists of Places objects that go with each.
-var listings = [];
-
-// Place constructor that will give us an object for each place category that contains the results list of places.
-function Place (results, name, icon) {
+// Listing constructor that will give us an object for each place category that contains the results list of places.
+function Listing (results, name, icon) {
   this.results = results;
   this.name = name;
   this.icon = icon;
 };
 
+
 var ViewModel =  function() {
   var self = this;
-  this.listings = ko.observableArray([]);
 
-  // Get our place listings and push to ko observableArray.
-  listings.forEach(function (listing) {
-            if (listings.length < categories.length) {
-              setTimeout(wait,100);
-              }
-            else {
-              self.listings.push(listing);
-              }
-          });
+  // Place to store our listings for each category
+  this.listings = new ko.observableArray([]);
+
+  // Place to store our markers
+  this.markers = new ko.observableArray([]);
+
+  // Place to store our names that will appear in the list view, one to store all places and one that updates with live search.
+  this.listNamesControl = new ko.observableArray([]);
+  this.listNamesLive = new ko.observableArray([]);
+
+  // Show or hide the set of markers on the map for a specific category
+  this.toggleMarkers = function () {
+    var markers = self.markers;
+    var listingIcon = this.icon;
+    markers().forEach(function(marker) {
+      var markerIcon = marker.icon.url;
+      var markerTitle = marker.title;
+      if (listingIcon === markerIcon) {
+
+        if (marker.visible !== false) {
+          marker.setVisible(false);
+
+          // We'll also make sure the live search list view is updated to be in sync with the displayed markers.
+          self.removeFromListView(markerTitle);
+        }
+        else {
+          marker.setVisible(true);
+
+          // We'll also make sure the live search list view is updated to be in sync with the displayed markers.
+          self.addToListView(markerTitle);
+        }
+      }
+
+    });
+
+
+
+  }
+
+  // Takes places from our categories places list and removes them from the list view. This executes within our toggleMarkers() function whenever we are removing places from the map.
+  this.removeFromListView = function(markerTitle) {
+    var listNamesLive = self.listNamesLive;
+    listNamesLive().forEach(function(listPlaceName) {
+      if (listPlaceName === markerTitle) {
+        var index = listNamesLive.indexOf(listPlaceName);
+        listNamesLive.splice(index, 1);
+      }
+    });
+  };
+
+  // Takes places from our categories places list and removes them from the list view. This executes within our toggleMarkers() function whenever we are adding places back to the map.
+  this.addToListView = function(markerTitle) {
+    var listNamesLive = self.listNamesLive;
+        listNamesLive.push(markerTitle);
+        listNamesLive.sort();
+  };
+
+  this.query = ko.observable('');
+
+  // Match value from our search input to our list of places to create our live search list view.
+  this.search = function(value) {
+    var listNamesControl = self.listNamesControl;
+    var listNamesLive = self.listNamesLive;
+    listNamesLive.removeAll();
+
+    listNamesControl().forEach(function (name) {
+      if (name.toLowerCase().indexOf(value.toLowerCase()) >= 0) {
+        listNamesLive.push(name);
+      }
+    });
+  }
+
+  this.activateMarker  = function() {
+    var markers = self.markers;
+    var placeName = this;
+    markers().forEach(function(marker) {
+      var markerTitle = marker.title;
+      console.log(markerTitle);
+      console.log(placeName);
+      if (markerTitle === placeName) {
+
+        makeActiveMarker();
+        }
+    });
+
+  }
 
 };
 
-ko.applyBindings(new ViewModel());
+var vm = new ViewModel();
+ko.applyBindings(vm);
+vm.query.subscribe(vm.search);
 
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
@@ -65,25 +141,51 @@ function initMap() {
        if (status === google.maps.places.PlacesServiceStatus.OK) {
          var name = category.name;
          var icon = category.iconURL;
-            createMarkersForPlaces(results, icon);
 
-            // Create a place object for each category that includes the text search results.
-            var place = new Place(results, name, icon);
-            listings.push(place);
+        // Get the places from our results and add markers to the map
+        createMarkersForPlaces(results, icon);
+
+        // Get the place names from our results list and we will push them to a KO observableArray. This will help us create our list view of place names that refreshes based on the live search feature.
+        createListNames(results);
+
+        // Create a listing object for each category that includes the text search results.
+        var listing = new Listing(results, name, icon);
+        vm.listings.push(listing);
+      }
+      else {
+        alert("Places request was not successful for the following reasons: " + status);
       }
      });
    });
 
- };
 
-  function createMarkersForPlaces(places, categoryIcon) {
+   };
+
+   textSearchPlaces();
+
+   // Create a single infowindow to be used with the place details information
+   // so that only one is open at once.
+   var placeInfoWindow = new google.maps.InfoWindow();
+
+  // Create list of place names that we will use for our list view.
+  function createListNames(results) {
+    results.forEach(function(result) {
+      var name = result.name;
+      vm.listNamesControl.push(name);
+      vm.listNamesLive.push(name);
+      vm.listNamesLive.sort();
+    })
+  }
+
+
+  function createMarkersForPlaces(places, iconURL) {
     var bounds = new google.maps.LatLngBounds();
     for (var i = 0; i < places.length; i++) {
       var place = places[i];
       var icon = {
 
         // Replace icon's url with the iconURL we passed in from our categories array.
-        url: categoryIcon,
+        url: iconURL,
 
         size: new google.maps.Size(40, 40),
         origin: new google.maps.Point(0, 0),
@@ -94,23 +196,26 @@ function initMap() {
       var marker = new google.maps.Marker({
         map: map,
         icon: icon,
+        visible: true,
         title: place.name,
         position: place.geometry.location,
         id: place.place_id
       });
 
-      // Create a single infowindow to be used with the place details information
-      // so that only one is open at once.
-      var placeInfoWindow = new google.maps.InfoWindow();
-      // If a marker is clicked, do a place details search on it in the next function.
-      marker.addListener('click', function() {
+      vm.markers.push(marker);
+
+      // If a marker is clicked, get its place details and display in infowindow.
+      marker.addListener('click', makeActiveMarker);
+
+      var makeActiveMarker = function () {
+        this.setAnimation(google.maps.Animation.DROP);
         if (placeInfoWindow.marker == this) {
-          console.log("This infowindow already is on this marker!");
+         console.log("This infowindow already is on this marker!");
         } else {
-          getPlacesDetails(this, placeInfoWindow);
+         getPlacesDetails(this, placeInfoWindow);
         }
-      });
-      // placeMarkers.push(marker);
+      };
+
       if (place.geometry.viewport) {
         // Only geocodes have viewport.
         bounds.union(place.geometry.viewport);
@@ -141,6 +246,9 @@ function getPlacesDetails(marker, infowindow) {
         innerHTML += '<br><br><img src="' + place.photos[0].getUrl(
             {maxHeight: 100, maxWidth: 200}) + '">';
       }
+      if (place.photos === null) {
+        innerHTML += '<br><br>Sorry, there is no photo to show for this place'
+      }
       innerHTML += '</div>';
       infowindow.setContent(innerHTML);
       infowindow.open(map, marker);
@@ -152,9 +260,16 @@ function getPlacesDetails(marker, infowindow) {
   });
 }
 
-textSearchPlaces();
-}
+ }
 
 
-setTimeout(function(){ console.log(listings); }, 3000);
-setTimeout(function(){ console.log(ViewModel.listings()); }, 6000);
+
+
+
+
+
+
+
+setTimeout(function(){ console.log(vm.markers()); }, 4000);
+setTimeout(function(){ console.log(vm.listings()); }, 4000);
+// setTimeout(function(){ console.log(vm.listNames()); }, 4000);
